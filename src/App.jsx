9b570a6d8e56c698f8365aa9503a0074f1dee3e6
joaovitorlopes <joaovitorlopes914@gmail.com>
2023-04-19 16:19:17 -0300

    criado componente cover

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Cover from './components/Cover';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return(
+      <div className="h-screen w-screen bg-zinc-800 overflow-x-hidden">
+          <Cover image="https://www.10wallpaper.com/wallpaper/3840x2160/1804/2018_Avengers_Infinity_War_4K_Film_3840x2160.jpg" title={"Vingadores: Guerra Infinita"} overview={"Homem de Ferro, Capitão América, Thor, Hulk e os Vingadores se unem para combater o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a humanidade."}/>
+          <h1>teste</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
-
-export default App
---
+++ src/components/Cover.jsx
@@ -0,0 +1,17 @@
+export default function Cover({image, title, overview}){
+    return(
+        <div className="bg-zinc-300 h-screen w-screen relative" style={{
+            background: `url('${image}')`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}>
+
+            <div className="absolute bg-gradient-to-tr from-zinc-900/95 to-zinc-900/0 h-full w-full flex flex-col pt-52 pl-10">
+                <h1 className="text-5xl font-bold text-zinc-200">{title}</h1>
+                <h1 className="text-2xl font-medium text-zinc-200 w-1/2 mt-7">{overview}</h1>
+            </div>
+        
+        </div>
+    )
+}
