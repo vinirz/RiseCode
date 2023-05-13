8a7874fbd56cb1cc853b03b1d2cf5f4f0dfeb74b
MatheusLamarca <matheus.ferreira.lamarca@gmail.com>
2023-05-13 10:25:09 -0300

    Add Categories

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,8 +1,18 @@
-export default function Categories(){
+import Card from './../components/Card';
 
+export default function Categories(){
     return(
-        <div className="flex flex-col items-center justify-center gap-10 w-screen">
-            <h1>Categories</h1>
+        <div className="p-10 w-screen overflow-auto flex flex-col relative">
+            <h1 className='text-3xl text-zinc-100 mb-3'>Ação</h1>
+            <div className="w-full h-1/2 flex gap-3 p-3 overflow-auto">
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+                <span className='w-52 flex-shrink-0 flex-grow-0 bg-emerald-500/70 rounded-md'></span>
+            </div>
+
         </div>
     )
 }
