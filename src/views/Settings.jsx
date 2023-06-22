c955fe4c6ad4202e1c663ecc06639e72fc2909b5
Vinicius Reis <vrzotech@gmail.com>
2023-06-22 17:06:19 -0300

    Alteração dados usuario

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -1,20 +1,34 @@
+import { createRef, useState } from "react"
+import { User, UpdateUser } from "../Utils"
 import { useNavigate } from "react-router-dom"
 
 export default function SettingsView(){   
 
+    const auth = User.getUser()
     const navigate = useNavigate()
+    const formRef = createRef()
+    const [pic, setPic] = useState(auth.picture)    
+
+    async function updateInfos(){
+            const name =  formRef.current.name.value
+            const mail =  formRef.current.mail.value
+            const password =  formRef.current.password.value
+            const pic =  formRef.current.pic.files[0] 
+
+        UpdateUser(name, mail, password, pic)
+    }
 
     return(
         <div className="p-10 w-screen h-screen">
-            <h1 className="text-zinc-200 text-3xl mb-5">Diggo</h1>
+            <h1 className="text-zinc-200 text-3xl mb-5">{auth.name}</h1>
             <div className="flex flex-col items-center justify-center gap-10 w-[100%] h-[90%]">
-                <form className="flex flex-col gap-5 items-center text-zinc-100" onSubmit={(e) => {e.preventDefault()}}>
-                    <input type="file" className="hidden" id="pic"/>
+                <form className="flex flex-col gap-5 items-center text-zinc-100" onSubmit={(e) => { e.preventDefault(), updateInfos()}} ref={formRef}>
+                    <input type="file" className="hidden" id="pic" onInputCapture={(e)=>(setPic(URL.createObjectURL(e.target.files[0])))}/>
                     <label htmlFor="pic">
-                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoHNU9U2B7PIoOMkNDKQEhRIdZQbscODfhQ&usqp=CAU' className="overflow-hidden h-32 w-32 bg-zinc-50 rounded-full object-cover"/>
+                        <img src={pic} className="overflow-hidden h-32 w-32 bg-zinc-50 rounded-full object-cover"/>
                     </label>
-                    <input type="text" placeholder="Nome" id="name" className="h-11 p-5 w-80 rounded-full bg-zinc-500 placeholder:text-zinc-200"/>
-                    <input type="email" placeholder="Email" id="mail" className="h-11 p-5 w-80 rounded-full bg-zinc-500 placeholder:text-zinc-200"/>
+                    <input type="text" placeholder="Nome" id="name" defaultValue={auth.name}  className="h-11 p-5 w-80 rounded-full bg-zinc-500 placeholder:text-zinc-200"/>
+                    <input type="email" placeholder="Email" id="mail" defaultValue={auth.email} className="h-11 p-5 w-80 rounded-full bg-zinc-500 placeholder:text-zinc-200"/>
                     <input type="password" placeholder="Nova Senha" id="password" className="h-11 p-5 w-80 rounded-full bg-zinc-500 placeholder:text-zinc-200"/>
 
                     <div>
@@ -24,7 +38,7 @@ export default function SettingsView(){
                                 <h1>Salvar alterações</h1>
                             </span>
                         </button>
-                        <button className="bg-zinc-500 mt-5 w-72 h-11 group rounded-full flex items-center justify-center overflow-hidden relative" onClick={() => {localStorage.clear(), window.location.reload()}}>
+                        <button className="bg-zinc-500 mt-5 w-72 h-11 group rounded-full flex items-center justify-center overflow-hidden relative" onClick={() => {navigate('/'), localStorage.removeItem('user')}}>
                             <span className='absolute bg-red-400 left-[-900px] group-hover:left-0 h-full w-full transition-all'></span>
                             <span className='z-10 flex gap-4 text-xl items-center justify-center text-zinc-200 group-hover:text-red-800'>
                                 <h1>Sair</h1>
--- src/views/Trend.jsx
+++ src/views/Trend.jsx
@@ -1,8 +1,47 @@
+import Card from "../components/Card"
+import Airtable from 'airtable';
+import { useEffect, useState } from 'react';
+
 export default function Trend(){
 
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+      const base = new Airtable({ apiKey: 'keymZeqCHaPAJcsxx' }).base('appWSfcNcPT8jjHkD');
+      
+      base('Videos').select({
+          view: 'Grid view',
+          sort: [
+            {field: "views", direction: "desc"}
+        ]
+      }).firstPage(function(err, records) {
+          if (err) { console.error(err); return; }
+          records.forEach(function(record) {
+              setMovies(movies => [...movies, record.fields])
+          });
+      });
+  
+    }, []);
+
     return(
-        <div className="flex flex-col items-center justify-center gap-10 w-screen">
-            <h1>Trend</h1>
-        </div>
+        <div className="flex flex-col p-10 w-screen h-full gap-8 overflow-auto text-zinc-200">
+                <h1 className="text-zinc-200 text-3xl">Populares</h1>
+                {
+                    movies.length != 0 && (movies.map((movie, index) => {
+                        return (
+                            <div className="grid grid-cols-3 gap-5 w-full 2xl:flex" key={index}>
+                                <div className="col-span-1 2xl:w-96">
+                                    <Card video={movie.url} channel={movie.channel} id={movie.ID}/>
+                                </div>
+                                <span className="flex flex-col justify-evenly col-span-2">
+                                    <h1 className="text-3xl font-semibold">{movie.title}</h1>
+                                    <h1 className="text-xl opacity-50 h-20 overflow-hidden">{movie.description.length > 220 ? movie.description.slice(0,218) + '...' : movie.description}</h1>
+                                    <h1 className="text-lg font-semibold">{movie.views + ' views • ' + movie.date}</h1>
+                                </span>
+                            </div>
+                        )
+                    }))
+                }
+            </div>
     )
 }
